eca9f4f6227dcaf4cc92247609f986b7b3e9c3cb
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-02-28 04:34:50 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, inclusion: { in: %w(Fiction Non-fiction), message: "%{category} is not valid"}
+  validates :category, inclusion: { in: %w(Fiction Non-fiction)}
   validates :content, length: { minimum: 100}
 end
